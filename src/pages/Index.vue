a7f9a4dad6e7a6432ce354075d0c20d307185094
earthtone82 <antonio.hubilla@pizzahutaustralia.com.au>
2019-08-13 16:43:33 +1000

    Update Styles and Configure Deployment
    - setup travis config
    - add silly CSS animation

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <transition enter-active-class="animated fadeIn">
       <div v-if="display" class="px-6 max-w-3 mx-auto md:px-12 flex flex-col justify-center h-full -mt-12">
         <p class="text-2xl md:text-3xl lg:text-5xl font-hairline font-serif leading-tight">{{ name }}</p>
-        <p class="md:text-md font-hairline self-end">{{title}}</p>
+        <p class="title-text md:text-md font-hairline self-end">{{title}}</p>
       </div>
     </transition>
   </Layout>
@@ -27,11 +27,24 @@ export default {
 </script>
 
 <style scoped>
+/*
+  Augment tailwindcss for more fine-grained
+  control and native CSS animations
+ */
+
 #app {
   background-image: url("~@/assets/img/DSC01414.png");
   background-position: bottom center;
   background-size: contain;
   background-attachment: fixed;
   background-repeat: no-repeat;
+}
+
+.title-text {
+  transition: transform 800ms ease-in-out;
+}
+
+.title-text:hover {
+  transform: scale(1.5) rotate(10deg);
 }
 </style>
